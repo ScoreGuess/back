24802db6d6b427160d24f11254c1331d6ec8c85e
Charles <charles@medeo-health.com>
2020-09-27 20:44:32 +0200

    decodeURI

--- functions/Groups/resolvers.js
+++ functions/Groups/resolvers.js
@@ -1,48 +1,51 @@
-const { save, find,findOne, findOneAndUpdate } = require("../utils/db");
+const {save, find, findOne, findOneAndUpdate} = require("../utils/db");
 const admin = require("firebase-admin");
 
-const create = async (_, resource, { userId }) => {
-  return await save("groups", {
-    ...resource,
-    participants: [userId],
-    author: userId,
-  });
+const create = async (_, resource, {userId}) => {
+    return await save("groups", {
+        ...resource,
+        participants: [userId],
+        author: userId,
+    });
 };
-const join = async (_, { groupId, userId },context) => {
-  userId = userId ==null ?  context.userId:userId
-  return await findOneAndUpdate("groups", groupId, (group) => ({
-    ...group,
-    participants: [...group.participants, userId].filter(
-      (elem, pos, arr) => arr.indexOf(elem) === pos
-    ),
-  }));
-};
-
-const search = async (_, __, { userId }) => {
-  return await find("groups");
+const join = async (_, {groupId, userId}, context) => {
+    userId = userId == null ? context.userId : userId
+    return await findOneAndUpdate("groups", groupId, (group) => ({
+        ...group,
+        participants: [...group.participants, userId].filter(
+            (elem, pos, arr) => arr.indexOf(elem) === pos
+        ),
+    }));
 };
 
-const read = async (_, { groupId}, { userId }) => {
-  return await findOne("groups", groupId);
+const search = async (_, __, {userId}) => {
+    const groups = await find("groups");
+    return groups.filter(group => {
+        return group.author === userId || group.participants.some(participantId => participantId === userId)
+    })
+};
+
+const read = async (_, {groupId}, {userId}) => {
+    return await findOne("groups", groupId);
 };
 
 module.exports = {
-  create,
-  read,
-  search,
-  join,
-  Group: {
-    author: async (group)=>{
-      const user = await admin.auth().getUser(group.author)
-      return {...user, id: user.uid}
-    },
-    participants: async (group) => {
-      // c.f. https://firebase.google.com/docs/auth/admin/manage-users?authuser=0#retrieve_user_data
-      const { users } = await admin
-        .auth()
-        .getUsers(group.participants.map((p) => ({ uid: p })));
+    create,
+    read,
+    search,
+    join,
+    Group: {
+        author: async (group) => {
+            const user = await admin.auth().getUser(group.author)
+            return {...user, id: user.uid}
+        },
+        participants: async (group) => {
+            // c.f. https://firebase.google.com/docs/auth/admin/manage-users?authuser=0#retrieve_user_data
+            const {users} = await admin
+                .auth()
+                .getUsers(group.participants.map((p) => ({uid: p})));
 
-      return users.map((user) => ({ ...user, id: user.uid }));
+            return users.map((user) => ({...user, id: user.uid}));
+        },
     },
-  },
 };
